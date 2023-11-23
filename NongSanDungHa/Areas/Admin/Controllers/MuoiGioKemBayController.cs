37cf3a92fb7a9f5efaeef4d99bcc4a9529459821
Quang Dieu <boquangdieu2003@gmail.com>
2023-11-23 21:48:12 +0700

    muoigiokembay controller added

---
+++ NongSanDungHa/Areas/Admin/Controllers/MuoiGioKemBayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NongSanDungHa.Areas.Admin.Controllers
+{
+    public class MuoiGioKemBayController : Controller
+    {
+        // GET: Admin/MuoiGioKemBay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
